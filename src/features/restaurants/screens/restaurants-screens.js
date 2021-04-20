70b3ffa36079bfc5ef2a0ac34bcea9ec04481d2f
Your Name <tojuofe>
2021-04-20 15:38:21 +0100

    built UI for Meal

---
+++ src/features/restaurants/screens/restaurants-screens.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import styled from "styled-components/native";
+import { StatusBar, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+import { RestaurantInfo } from "../components/restaurants-info-card.component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantScreen = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantInfo />
+      </RestaurantListContainer>
+    </SafeArea>
+  );
+};
